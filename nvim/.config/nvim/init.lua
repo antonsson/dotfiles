7baf74fe172d9c6f55551d2973b6984f91b49e89
Henrik Antonsson <henrik.antonsson@gmail.com>
2021-07-12 19:29:15 +0200

    nvim: lua configs in separate folder

--- nvim/.config/nvim/init.lua
+++ nvim/.config/nvim/init.lua
@@ -1,19 +1,19 @@
-local utils = require('utils')
+local utils = require"utils"
 
 --------------------------------------------------------------------------------
 -- General neovim configuration
 --------------------------------------------------------------------------------
 -- Map leader to space
-vim.g.mapleader = ','
+vim.g.mapleader = ","
 
 -- More color to the people
 vim.opt.termguicolors = true
 
 -- Enable more mouse controls
-vim.opt.mouse = 'a'
+vim.opt.mouse = "a"
 
 -- Backspace to remove indents
-vim.opt.backspace = {'indent', 'eol', 'start'}
+vim.opt.backspace = {"indent", "eol", "start"}
 vim.opt.smartcase = true
 
 -- Scroll before reaching end
@@ -28,7 +28,7 @@ vim.opt.expandtab = true
 vim.opt.hlsearch = true
 vim.opt.incsearch = true
 vim.opt.ignorecase = true
-vim.opt.inccommand = 'nosplit'
+vim.opt.inccommand = "nosplit"
 
 -- Undo settings
 vim.opt.undofile = true
@@ -39,18 +39,18 @@ vim.opt.undoreload = 10000
 vim.opt.hidden = true
 
 -- Completion options
-vim.opt.completeopt = {'menuone', 'noselect'}
+vim.opt.completeopt = {"menuone", "noselect"}
 
 --------------------------------------------------------------------------------
 -- Packer
 --------------------------------------------------------------------------------
 
 -- Auto install packer.nvim if not exists
-local install_path = vim.fn.stdpath('data') ..
-                         '/site/pack/packer/opt/packer.nvim'
+local install_path = vim.fn.stdpath("data") ..
+                         "/site/pack/packer/opt/packer.nvim"
 if vim.fn.empty(vim.fn.glob(install_path)) > 0 then
     vim.api.nvim_command(
-        '!git clone https://github.com/wbthomason/packer.nvim ' .. install_path)
+        "!git clone https://github.com/wbthomason/packer.nvim " .. install_path)
 end
 vim.cmd [[packadd packer.nvim]]
 
@@ -61,69 +61,69 @@ local setup = function(mod)
     end
 end
 
-require('packer').startup(function(use)
+require("packer").startup(function(use)
 
     -- Packer can manage itself as an optional plugin
-    use {'wbthomason/packer.nvim', opt = true}
+    use {"wbthomason/packer.nvim", opt = true}
 
     -- Lir file explorer
-    use {'tamago324/lir.nvim', requires = {{'nvim-lua/plenary.nvim'}}}
+    use {"tamago324/lir.nvim", requires = {{"nvim-lua/plenary.nvim"}}}
 
     -- Pimped status line
     use {
-        'hoob3rt/lualine.nvim',
-        requires = {'kyazdani42/nvim-web-devicons', opt = true}
+        "hoob3rt/lualine.nvim",
+        requires = {"kyazdani42/nvim-web-devicons", opt = true}
     }
 
     -- Toggle quick/location list
-    use {'milkypostman/vim-togglelist'}
+    use {"milkypostman/vim-togglelist"}
 
     -- Color scheme and highlighter
-    use {'antonsson/equinusocio-material.vim'}
-    use {'antonsson/onedark.nvim'}
-    use {'norcalli/nvim-colorizer.lua', config = setup('colorizer')}
+    use {"antonsson/equinusocio-material.vim"}
+    use {"antonsson/onedark.nvim"}
+    use {"norcalli/nvim-colorizer.lua", config = setup("colorizer")}
 
     -- git
-    use {'airblade/vim-gitgutter'}
-    use {'tpope/vim-fugitive'}
+    use {"airblade/vim-gitgutter"}
+    use {"tpope/vim-fugitive"}
 
     -- Comment lines
-    use {'tpope/vim-commentary'}
+    use {"tpope/vim-commentary"}
 
     -- Syntax
-    use {'gburca/vim-logcat'}
+    use {"gburca/vim-logcat"}
 
     -- Treesitter
-    use {'nvim-treesitter/nvim-treesitter', run = ':TSUpdate'}
-    use {'nvim-treesitter/nvim-treesitter-textobjects'}
+    use {"nvim-treesitter/nvim-treesitter", run = ":TSUpdate"}
+    use {"nvim-treesitter/nvim-treesitter-textobjects"}
 
     -- Neovim bultin lsp
-    use {'neovim/nvim-lspconfig'}
-    use {'hrsh7th/nvim-compe'}
-    use {'folke/lsp-trouble.nvim', config = setup('trouble')}
-    use {'nvim-lua/lsp_extensions.nvim'}
-    use {'ray-x/lsp_signature.nvim'}
+    use {"neovim/nvim-lspconfig"}
+    use {"hrsh7th/nvim-compe"}
+    use {"folke/lsp-trouble.nvim", config = setup("trouble")}
+    use {"nvim-lua/lsp_extensions.nvim"}
+    use {"ray-x/lsp_signature.nvim"}
 
     -- Fuzzy search
-    use {'junegunn/fzf', dir = '~/.fzf', run = './install --all'}
-    use {'junegunn/fzf.vim'}
-    use {'gfanto/fzf-lsp.nvim', config = setup('fzf_lsp')}
+    use {"junegunn/fzf", dir = "~/.fzf", run = "./install --all"}
+    use {"junegunn/fzf.vim"}
+    use {"gfanto/fzf-lsp.nvim", config = setup("fzf_lsp")}
 
     -- Format multiple file types
-    use {'sbdchd/neoformat'}
+    use {"sbdchd/neoformat"}
 
     -- Show indentation line
-    use {'Yggdroot/indentLine'}
+    use {"Yggdroot/indentLine"}
 
     -- Preview markdown
-    use {'npxbr/glow.nvim'}
+    use {"npxbr/glow.nvim"}
 
     -- Focus mode
-    use {'folke/zen-mode.nvim', config = setup('zen-mode')}
+    use {"folke/zen-mode.nvim", config = setup("zen-mode")}
 end)
 
 -- Highlight yanked text
-vim.cmd [[autocmd TextYankPost * silent! lua require'vim.highlight'.on_yank()]]
+vim.cmd [[autocmd TextYankPost * silent! lua require"vim.highlight".on_yank()]]
 
 -- Makefiles should use tabulators
 vim.cmd [[autocmd FileType make setlocal shiftwidth=4 tabstop=4 noexpandtab]]
@@ -132,34 +132,34 @@ vim.cmd [[autocmd FileType make setlocal shiftwidth=4 tabstop=4 noexpandtab]]
 vim.cmd [[autocmd FileType yaml setlocal tabstop=2 softtabstop=2 shiftwidth=2 expandtab]]
 
 -- Copy to clipboard
-utils.map('v', '<leader>y', '\"+y')
+utils.map("v", "<leader>y", '"+y')
 
 -- Highlight yanked text
 vim.cmd [[augroup LuaHighlight]]
 vim.cmd [[  autocmd!]]
-vim.cmd [[  autocmd TextYankPost * silent! lua require'vim.highlight'.on_yank()]]
+vim.cmd [[  autocmd TextYankPost * silent! lua require"vim.highlight".on_yank()]]
 vim.cmd [[augroup END]]
 
 -- Navigation
-utils.map('n', '.', '.`[')
-utils.map('n', 'j', 'gj')
-utils.map('n', 'k', 'gk')
-utils.map('n', '<c-h>', ':ClangdSwitchSourceHeader <cr>')
-utils.map('n', '<F4>', ':e ~/.config/nvim/init.lua <cr>')
+utils.map("n", ".", ".`[")
+utils.map("n", "j", "gj")
+utils.map("n", "k", "gk")
+utils.map("n", "<c-h>", ":ClangdSwitchSourceHeader <cr>")
+utils.map("n", "<F4>", ":e ~/.config/nvim/init.lua <cr>")
 
 -- Format file
-utils.map('n', '<leader>cf', ':Neoformat<CR>')
-utils.map('v', '<leader>cf', ':Neoformat<CR>')
+utils.map("n", "<leader>cf", ":Neoformat<CR>")
+utils.map("v", "<leader>cf", ":Neoformat<CR>")
 
 -- For easier searching
-utils.map('n', '-', '/')
-utils.map('n', '_', '?')
+utils.map("n", "-", "/")
+utils.map("n", "_", "?")
 
 --------------------------------------------------------------------------------
 -- Color scheme
 --------------------------------------------------------------------------------
-vim.g.onedark_style = 'blackhole'
-require('onedark').setup()
+vim.g.onedark_style = "blackhole"
+require("onedark").setup()
 
 --------------------------------------------------------------------------------
 -- indentline
@@ -178,28 +178,28 @@ vim.g.gitgutter_sign_priority = 0
 --------------------------------------------------------------------------------
 -- Prioritize eslint-formatter for javascript
 vim.g.neoformat_enabled_javascript = {
-    'prettiereslint', 'jsbeautify', 'clang-format'
+    "prettiereslint", "jsbeautify", "clang-format"
 }
-vim.g.neoformat_enabled_python = {'autopep8', 'yapf', 'docformatter'}
+vim.g.neoformat_enabled_python = {"autopep8", "yapf", "docformatter"}
 
 --------------------------------------------------------------------------------
 -- fzf
 --------------------------------------------------------------------------------
-vim.fn.setenv('FZF_DEFAULT_OPTS', '--layout=reverse --margin=1,3')
-vim.api.nvim_set_var('fzf_layout', {window = {width = 0.8, height = 0.8}})
-vim.api.nvim_set_var('fzf_preview_window', {'up:40%', 'ctrl-/'})
-utils.map('n', '<leader>f', ':Files<cr>')
-utils.map('n', '<leader>b', ':Buffers <cr>')
-utils.map('n', '<leader>j', ':GFiles <cr>')
-utils.map('n', '<leader>g', ':Rg <cr>')
-utils.map('n', '<leader>G', ':Rg <c-r><c-w><cr>')
+vim.fn.setenv("FZF_DEFAULT_OPTS", "--layout=reverse --margin=1,3")
+vim.api.nvim_set_var("fzf_layout", {window = {width = 0.8, height = 0.8}})
+vim.api.nvim_set_var("fzf_preview_window", {"up:40%", "ctrl-/"})
+utils.map("n", "<leader>f", ":Files<cr>")
+utils.map("n", "<leader>b", ":Buffers <cr>")
+utils.map("n", "<leader>j", ":GFiles <cr>")
+utils.map("n", "<leader>g", ":Rg <cr>")
+utils.map("n", "<leader>G", ":Rg <c-r><c-w><cr>")
 
 --------------------------------------------------------------------------------
 -- External plugin configs
 --------------------------------------------------------------------------------
-require("lsp_config")
-require("lir_config")
-require("compe_config")
-require("lualine_config")
-require("treesitter_config")
+require("config.lsp")
+require("config.lir")
+require("config.compe")
+require("config.lualine")
+require("config.treesitter")
 
