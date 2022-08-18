e38695597257c0dfd8839a901522e3c69a173de9
Henrik Antonsson <henrik.antonsson@gmail.com>
2022-08-18 10:46:55 +0200

    wezterm: add default config

---
+++ wezterm/.config/wezterm/wezterm.lua
@@ -0,0 +1,9 @@
+local wezterm = require 'wezterm'
+
+return {
+    font = wezterm.font 'Source Code Pro Medium',
+    font_size = 15,
+    color_scheme = 'My Material',
+    hide_tab_bar_if_only_one_tab = true,
+    scrollback_lines = 3500,
+}
