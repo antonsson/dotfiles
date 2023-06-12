d5c7cec8efef9c1394af2430e27c1fddcb8e475a
Henrik Antonsson <henrik.antonsson@gmail.com>
2023-06-12 12:19:24 +0200

    nvim: update diagnostic signs

--- nvim/.config/nvim/init.lua
+++ nvim/.config/nvim/init.lua
@@ -582,7 +582,7 @@ require("lazy").setup({
                 severity_sort = false
             })
 
-            local signs = {Error = "", Warn = "", Hint = " ", Info = ""}
+            local signs = {Error = "", Warn = "", Hint = "", Info = ""}
             for type, icon in pairs(signs) do
                 local hl = "DiagnosticSign" .. type
                 vim.fn.sign_define(hl, {text = icon, texthl = hl, numhl = hl})
