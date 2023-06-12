b00786a864ca9e7ca309a5296cfcf49f158d9a88
Henrik Antonsson <henrik.antonsson@gmail.com>
2023-06-12 12:19:53 +0200

    wezterm: fira code font and hyperlink shortcuts

--- wezterm/.config/wezterm/wezterm.lua
+++ wezterm/.config/wezterm/wezterm.lua
@@ -1,10 +1,29 @@
 local wezterm = require 'wezterm'
 
 return {
-    font = wezterm.font 'Source Code Pro Medium',
-    font_size = 15,
+    font =  wezterm.font {
+        family = 'FiraCode Nerd Font Ret',
+        harfbuzz_features = { 'calt=0', 'clig=0', 'liga=0' }
+    },
+    font_size = 14,
     color_scheme = 'My Material',
     hide_tab_bar_if_only_one_tab = true,
     scrollback_lines = 3500,
     enable_wayland = true,
+    keys = {
+        {
+            key = 'H',
+            mods = 'CTRL',
+            action = wezterm.action.QuickSelectArgs {
+                label = 'open url',
+                patterns = {'https?://\\S+'},
+                action = wezterm.action_callback(
+                    function(window, pane)
+                        local url = window:get_selection_text_for_pane(pane)
+                        wezterm.log_info('opening: ' .. url)
+                        wezterm.open_with(url)
+                    end)
+            }
+        }
+    }
 }
