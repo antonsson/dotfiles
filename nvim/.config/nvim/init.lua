1065fbc3e2e2b1c157ea1c41dc5088e5621b92e1
Henrik Antonsson <henrik.antonsson@gmail.com>
2023-07-05 10:35:53 +0200

    nvim: add flash plugin

--- nvim/.config/nvim/init.lua
+++ nvim/.config/nvim/init.lua
@@ -356,41 +356,29 @@ require("lazy").setup({
         end,
     },
 
-    -- Svart - jump to characters
+    -- Flash - search with tags
     {
-        "https://gitlab.com/madyanov/svart.nvim",
+        "folke/flash.nvim",
+        event = "VeryLazy",
+        opts = {},
         keys = {
-            {"-", "<Cmd>Svart<CR>", mode = "n"},
-            {"-", "<Cmd>Svart<CR>", mode = "v"},
+            {
+                "-",
+                mode = { "n", "x", "o" },
+                function()
+                    require("flash").jump()
+                end,
+                desc = "Flash",
+            },
+            {
+                "_",
+                mode = { "n", "o", "x" },
+                function()
+                    require("flash").treesitter()
+                end,
+                desc = "Flash Treesitter",
+            },
         },
-        config = function()
-            local svart = require("svart")
-
-            svart.configure({
-                key_cancel = "<Esc>", -- cancel search
-                key_delete_char = "<BS>", -- delete query char
-                key_delete_word = "<C-W>", -- delete query word
-                key_delete_query = "<C-U>", -- delete whole query
-                key_best_match = "<CR>", -- jump to the best match
-                key_next_match = "<C-N>", -- select next match
-                key_prev_match = "<C-P>", -- select prev match
-
-                label_atoms = "jfkdlsahgnuvrbytmiceoxwpqz", -- allowed label chars
-                label_location = -1, -- label location relative to the match
-                -- positive = relative to the start of the match
-                -- 0 or negative = relative to the end of the match
-                label_max_len = 2, -- max label length
-                label_min_query_len = 1, -- min query length required to show labels
-                label_hide_irrelevant = true, -- hide irrelevant labels after start typing label to go to
-                label_conflict_foresight = 3, -- number of chars from the start of the match to discard from labels pool
-
-                search_update_register = false, -- update search (/) register with last used query after accepting match
-                search_wrap_around = true, -- wrap around when navigating to next/prev match
-                search_multi_window = true, -- search in multiple windows
-
-                ui_dim_content = true -- dim buffer content during search
-            })
-        end,
     },
 
     -- Telescope
