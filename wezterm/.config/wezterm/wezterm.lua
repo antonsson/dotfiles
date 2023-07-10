1804a7a8e32b560f6ea51586c1a28cbb63103f75
Henrik Antonsson <henrik.antonsson@gmail.com>
2023-07-10 11:17:57 +0200

    wezterm: add tokyonight colors

--- wezterm/.config/wezterm/wezterm.lua
+++ wezterm/.config/wezterm/wezterm.lua
@@ -6,7 +6,7 @@ return {
         harfbuzz_features = { 'calt=0', 'clig=0', 'liga=0' }
     },
     font_size = 14,
-    color_scheme = 'My Material',
+    color_scheme = 'Tokyo Night Storm',
     hide_tab_bar_if_only_one_tab = true,
     scrollback_lines = 3500,
     enable_wayland = true,
