b8b4474e4ceb9ef453457c1ba4f7ac5ffa3fac1b
Henrik Antonsson <henrik.antonsson@gmail.com>
2024-03-18 14:58:16 +0100

    hyprland: update and fix config name

---
+++ hypr/.config/hyprland/scripts/sleep.sh
@@ -0,0 +1,4 @@
+chayang && swayidle -w \
+    timeout 3600 'systemctl suspend' \
+    before-sleep 'swaylock -f -c 000000'
+
--- hyprland/.config/hyprland/scripts/sleep.sh
+++
@@ -1,4 +0,0 @@
-exec chayang && swayidle -w \
-         timeout 3600 'systemctl suspend' \
-         before-sleep 'swaylock -f -c 000000'
-
